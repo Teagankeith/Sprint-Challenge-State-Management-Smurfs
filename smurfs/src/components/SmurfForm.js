29e56d545d983c117f3c6daa6a20aac33ba33ea5
Teagan Keith <teagankeith@outlook.com>
2020-06-12 12:43:24 -0400

    Lost

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,61 @@
+import React from "react"
+import {useState} from "react"
+import { addSmurf } from "../actions/index"
+// import {useForm} from "./hooks/useform"
+import { connect } from "react-redux"
+
+const SmurfForm = (props) => {
+    const[smurf, setSmurf] = useState({
+        name: "",
+        age: Number(""),
+        height: ""
+    })
+  
+    
+
+    const handleChanges = e => {
+        let name = e.target.name;
+        let value = e.target.value
+        setSmurf({...smurf, [name]: value})
+    
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const newSmurf = {
+            ...smurf
+        }
+        props.addSmurf(newSmurf)
+    }
+
+
+    return (
+        <div className="smurfForm">
+            <form onSubmit={handleSubmit}>
+            <label>Name:&nbsp;</label>
+            <input
+            type="text"
+            name="name"
+            onChange={handleChanges}
+            />
+            <label>Age:&nbsp;</label>
+            <input
+            type="text"
+            name="value"
+            onChange={handleChanges}
+            />
+            <label>Height:&nbsp;</label>
+            <input
+            type="text"
+            name="height"
+            onChange={handleChanges}
+            />
+
+            <button id="submit">Add Smurf</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default connect(null, {addSmurf})(SmurfForm)
